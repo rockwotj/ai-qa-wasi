f269aeef521a52aed38155a57194a2dc8fe50042
Alexander Gallego <alexandergallego@AlexandacStudio.localdomain>
2024-02-26 11:06:33 -0800

    full end to end demo

--- setup.sh
+++ setup.sh
@@ -1,10 +1,30 @@
+#!/usr/bin/env bash
+
+# e: errors, x: debug, u: only deefine vars, -o pipefail: don't mask errors
+set -euxo pipefail
+ 
+if false; then
+    brew install rust rustup redpanda-data/tap/redpanda
+fi
+
+# flag for rust install
+if false; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
 # Install python deps and export our model.
-pip3 install transformers torch torchinfo
-python3 export.py
+#pip3 install --user transformers torch torchinfo onnx
+#python3 export.py
+
+#add the wasi target
+rustup target add wasm32-wasi
+
 # Build it!
 RUSTFLAGS="-Ctarget-feature=+simd128" cargo build --release --target=wasm32-wasi
+
+rpk container purge
 # Deploy to our locally running container
-rpk container start
+rpk container start --image docker.redpanda.com/redpandadata/redpanda:v23.3.5 --set rpk.additional_start_flags="--smp=4"
 # Modify the required cluster configurations
 rpk cluster config set data_transforms_enabled true
 # NOTE: These limits allow for a single transform with half a GiB of memory.
@@ -20,4 +40,10 @@ rpk container start
 # Create needed topics
 rpk topic create questions answers
 cp ./target/wasm32-wasi/release/ai-qa-wasi.wasm .
-rpk wasm deploy
+rpk wasm deploy -v
+
+sleep 3
+echo "how many employees does JumpTrading employ?" | rpk topic produce questions
+echo "list the offices of jumptrading?" | rpk topic produce questions
+echo "who founded jumptrading?" | rpk topic produce questions
+echo "does jumptrading own a crypto firm?" | rpk topic produce questions
